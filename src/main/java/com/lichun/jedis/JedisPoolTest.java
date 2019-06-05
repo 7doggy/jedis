7509d5ba1a3b3aa2923704c3b1b15321262c78d4
7doggy <seven.doggy@gmail.com>
2019-06-05 16:07:05 +0800

    init

---
+++ src/main/java/com/lichun/jedis/JedisPoolTest.java
@@ -0,0 +1,24 @@
+package com.lichun.jedis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisPoolTest {
+    public static void main(String[] args) {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(100);
+        config.setMaxIdle(10);
+
+        JedisPool jedisPool = new JedisPool(config, "localhost", 6379);
+        try {
+            Jedis jedis = jedisPool.getResource();
+            jedis.auth("123456");
+            jedis.set("age", "35");
+            String age = jedis.get("age");
+            System.out.println("age:" + age);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
