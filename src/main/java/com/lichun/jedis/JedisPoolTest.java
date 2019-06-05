fa6fa0bdfff5db4e611164b7185a174b9344926a
7doggy <seven.doggy@gmail.com>
2019-06-05 16:08:36 +0800

    init

--- src/main/java/com/lichun/jedis/JedisPoolTest.java
+++ src/main/java/com/lichun/jedis/JedisPoolTest.java
@@ -11,14 +11,20 @@ public class JedisPoolTest {
         config.setMaxIdle(10);
 
         JedisPool jedisPool = new JedisPool(config, "localhost", 6379);
+        Jedis jedis = null;
         try {
-            Jedis jedis = jedisPool.getResource();
+            jedis = jedisPool.getResource();
             jedis.auth("123456");
             jedis.set("age", "35");
             String age = jedis.get("age");
             System.out.println("age:" + age);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+            jedisPool.close();
         }
     }
 }
